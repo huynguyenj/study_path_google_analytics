0bbf7161e86c0583c27dca8f858e79d0897bc5fc
huynguyenj <nguyenhuyjobs@gmail.com>
2025-10-28 09:16:26 +0700

    update

--- src/server.js
+++ src/server.js
@@ -7,11 +7,14 @@ export const START_SERVER = () => {
   const app = express()
   app.use(express.json())
   app.use(cors())
+  app.get('/', (req, res) => {
+    res.json({ message: 'Analytics API is running on Vercel!' });
+  });
   app.use('/analytics',RouteIndex)
-  app.listen(PORT, () => {
-      console.log(`Server connect to port ${PORT}`);
-  })
+  // app.listen(PORT, () => {
+  //     console.log(`Server connect to port ${PORT}`);
+  // })
 
 }
 
-START_SERVER()
+// START_SERVER()
