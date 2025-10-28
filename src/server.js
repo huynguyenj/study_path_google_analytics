ae89c49049ee02f82fccdeab29a1a47ca97b8e92
huynguyenj <nguyenhuyjobs@gmail.com>
2025-10-28 08:59:02 +0700

    update: server.js

--- src/server.js
+++ src/server.js
@@ -2,16 +2,19 @@ import express from 'express'
 import { envVariable } from './environments/env.js'
 import cors from 'cors'
 import { RouteIndex } from './router/router.js'
-const START_SERVER = () => {
+export const START_SERVER = () => {
   const PORT = envVariable.PORT ?? 3000
   const app = express()
   app.use(express.json())
   app.use(cors())
+  app.get('/', (req, res) => {
+    res.json({ message: 'Analytics API is running on Vercel!' });
+  });
   app.use('/analytics',RouteIndex)
-  app.listen(PORT, () => {
-      console.log(`Server connect to port ${PORT}`);
-  })
+  // app.listen(PORT, () => {
+  //     console.log(`Server connect to port ${PORT}`);
+  // })
 
 }
 
-START_SERVER()
+// START_SERVER()
