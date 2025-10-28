f78f035138b9f209a96f6694a0c37ecfcc0db115
huynguyenj <nguyenhuyjobs@gmail.com>
2025-10-28 14:29:46 +0700

    fix: vercel set up

--- src/server.js
+++ src/server.js
@@ -2,19 +2,29 @@ import express from 'express'
 import { envVariable } from './environments/env.js'
 import cors from 'cors'
 import { RouteIndex } from './router/router.js'
-export const START_SERVER = () => {
-  const PORT = envVariable.PORT ?? 3000
-  const app = express()
-  app.use(express.json())
-  app.use(cors())
-  app.get('/', (req, res) => {
-    res.json({ message: 'Analytics API is running on Vercel!' });
-  });
-  app.use('/analytics',RouteIndex)
-  // app.listen(PORT, () => {
-  //     console.log(`Server connect to port ${PORT}`);
-  // })
+// export const START_SERVER = () => {
+//   const PORT = envVariable.PORT ?? 3000
+//   const app = express()
+//   app.use(express.json())
+//   app.use(cors())
+//   app.use('/analytics',RouteIndex)
+//   app.listen(PORT, () => {
+//       console.log(`Server connect to port ${PORT}`);
+//   })
 
-}
+// }
 
 // START_SERVER()
+
+//Set up vercel functional
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Analytics API is running on Vercel!' })
+})
+
+app.use('/analytics', RouteIndex)
+
+export default app
